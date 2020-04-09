e5b6a7b432b959b2ef504ed8913a5fb95bbfadbc
Pallav Agarwal <agpallav@gmail.com>
2020-04-09 15:57:21 -0700

    Setup Add org View Controllers

---
+++ Purdue-CS-App/OrgCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrgCell.swift
+//  Purdue-CS-App
+//
+//  Created by Pallav Agarwal on 4/9/20.
+//  Copyright © 2020 Pallav Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class OrgCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Purdue-CS-App/SearchOrgsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchOrgsViewController.swift
+//  Purdue-CS-App
+//
+//  Created by Pallav Agarwal on 4/9/20.
+//  Copyright © 2020 Pallav Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class SearchOrgsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
