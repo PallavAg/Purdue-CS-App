197676e51391a9f27752b4ae51f21431a6235464
Pallav Agarwal <agpallav@gmail.com>
2020-04-10 12:53:48 -0700

    Added empty table prompt. Readded date limit.

--- Purdue-CS-App/OrgsViewController.swift
+++ Purdue-CS-App/OrgsViewController.swift
@@ -46,6 +46,39 @@ extension Date {
     }
 }
 
+extension UITableView {
+    func setEmptyView(title: String, message: String) {
+         let emptyView = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height))
+         let titleLabel = UILabel()
+         let messageLabel = UILabel()
+         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+         messageLabel.translatesAutoresizingMaskIntoConstraints = false
+         titleLabel.textColor = UIColor.lightGray
+         titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
+         messageLabel.textColor = UIColor.lightGray
+         messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+         emptyView.addSubview(titleLabel)
+         emptyView.addSubview(messageLabel)
+         titleLabel.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
+         titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+         messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+         messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 10).isActive = true
+         messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -10).isActive = true
+         titleLabel.text = title
+         messageLabel.text = message
+         messageLabel.numberOfLines = 0
+         messageLabel.textAlignment = .center
+         // The only tricky part is here:
+         self.backgroundView = emptyView
+         self.separatorStyle = .none
+     }
+    
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
+}
+
 //Calendar link to API
 func calendarIDtoAPI(calendar_id: String) -> String {
     let url = "https://www.googleapis.com/calendar/v3/calendars/" + calendar_id + "/events?maxResults=20&key=" + API.API_KEY
@@ -145,7 +178,7 @@ class OrgsViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
         
         //Remove all items more than 24hrs in the past
-        //tableEvents.removeAll (where: { getStartDate(event: $0).timeIntervalSinceNow < -86400 })
+        tableEvents.removeAll (where: { getStartDate(event: $0).timeIntervalSinceNow < -86400 })
         
         allResults = tableEvents
         
@@ -176,6 +209,7 @@ class OrgsViewController: UIViewController, UITableViewDelegate, UITableViewData
         if previousCals != calendar_ids {
             
             tableView.isScrollEnabled = false;
+            tableView.restore()
             self.myActivityIndicator.startAnimating()
             
             for (calendar_name, id) in calendar_ids  {
@@ -327,6 +361,13 @@ class OrgsViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableEvents.count == 0 {
+            tableView.setEmptyView(title: "No upcoming events.", message: "Tap the '+' icon to add organizations")
+        }
+        else {
+            tableView.restore()
+        }
+        
         return tableEvents.count
     }
     
